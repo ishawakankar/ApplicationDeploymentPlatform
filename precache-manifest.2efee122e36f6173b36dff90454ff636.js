4f81238b6b4404cd05956501ce7a779321d9c55f
ishawakankar <iwakankar@hotmail.com>
2019-02-04 13:16:54 +0530

    Updates

---
+++ precache-manifest.2efee122e36f6173b36dff90454ff636.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "11d38b71ab182e64a9440177f00030d4",
+    "url": "/static/media/gitlab.11d38b71.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1004b1852b5ab85a06ac",
+    "url": "/static/js/main.1004b185.chunk.js"
+  },
+  {
+    "revision": "80d88efa561ed38d3f44",
+    "url": "/static/js/1.80d88efa.chunk.js"
+  },
+  {
+    "revision": "1004b1852b5ab85a06ac",
+    "url": "/static/css/main.5923f361.chunk.css"
+  },
+  {
+    "revision": "98cbaf9f5aedf72b4b4d306173ff8348",
+    "url": "/index.html"
+  }
+];
---
+++ precache-manifest.8985dc6d7c2c30475fbb8b4d57d22c64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "11d38b71ab182e64a9440177f00030d4",
+    "url": "/static/media/gitlab.11d38b71.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3eae1ce3bd4153d47831",
+    "url": "/static/js/main.3eae1ce3.chunk.js"
+  },
+  {
+    "revision": "80d88efa561ed38d3f44",
+    "url": "/static/js/1.80d88efa.chunk.js"
+  },
+  {
+    "revision": "3eae1ce3bd4153d47831",
+    "url": "/static/css/main.5923f361.chunk.css"
+  },
+  {
+    "revision": "b40f888717307ef3d5ebfa0a5371a6bc",
+    "url": "/index.html"
+  }
+];
